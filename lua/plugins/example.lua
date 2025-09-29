a156345d6772ac9e6d6ecddd973a8727930585fd
vii33 <https://x.com/vii33_official>
2025-09-29 20:24:54 +0200

    renamed example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,11 +1,10 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
+
 return {
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- default list is EXTENDED (if you want to replace it, see below)
+      -- default list is EXTENDED (if you want to replace it, see example.lua.bak file)
       vim.list_extend(opts.ensure_installed, {
         "bash",
         "html",
